85f6fc1231b7ef05a3a891d81b0f7cce4a591cb6
felipe@2far.com.br <felipe@2far.com.br>
2019-02-07 17:26:52 -0200

    First commit

---
+++ src/main/java/com/toofar/springmongo/SpringmongoApplication.java
@@ -0,0 +1,14 @@
+package com.toofar.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongoApplication.class, args);
+	}
+
+}
+
