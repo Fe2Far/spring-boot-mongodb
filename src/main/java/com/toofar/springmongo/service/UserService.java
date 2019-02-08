ab72880fd336a3c58317e13c11e87ea0e46ff003
felipe@2far.com.br <felipe@2far.com.br>
2019-02-08 12:57:10 -0200

    Busca usuários por ID
    Signed-off-by: felipe@2far.com.br <felipe@2far.com.br>

--- src/main/java/com/toofar/springmongo/service/UserService.java
+++ src/main/java/com/toofar/springmongo/service/UserService.java
@@ -1,21 +1,28 @@
 package com.toofar.springmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.toofar.springmongo.domain.User;
+import com.toofar.springmongo.exception.ObjectNotFoundException;
 import com.toofar.springmongo.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id : " + id));
 	}
 
 }
