3d4b4382b3c5fd203a8e801828e4cba131a58c56
felipe@2far.com.br <felipe@2far.com.br>
2019-02-08 13:13:17 -0200

    Insert Users
    Signed-off-by: felipe@2far.com.br <felipe@2far.com.br>

--- src/main/java/com/toofar/springmongo/service/UserService.java
+++ src/main/java/com/toofar/springmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.toofar.springmongo.domain.User;
+import com.toofar.springmongo.dto.UserDTO;
 import com.toofar.springmongo.exception.ObjectNotFoundException;
 import com.toofar.springmongo.repository.UserRepository;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id : " + id));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
