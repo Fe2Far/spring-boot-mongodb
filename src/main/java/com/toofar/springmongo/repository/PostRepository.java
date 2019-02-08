3b62ba8a8b41aa194140bb065aba60cadb7a4fd0
felipe@2far.com.br <felipe@2far.com.br>
2019-02-08 19:15:26 -0200

    Consulta simples com @query
    Signed-off-by: felipe@2far.com.br <felipe@2far.com.br>

--- src/main/java/com/toofar/springmongo/repository/PostRepository.java
+++ src/main/java/com/toofar/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.toofar.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.toofar.springmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.toofar.springmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
